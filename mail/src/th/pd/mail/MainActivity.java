4f3fc69cfa9f34b8efad5c3519adf72038d6d0b3
tanghao <tanghao@jidemail.com>
2015-05-22 15:29:02 +0800

    make Hedwig move

--- mail/src/th/pd/mail/MainActivity.java
+++ mail/src/th/pd/mail/MainActivity.java
@@ -3,14 +3,37 @@ package th.pd.mail;
 import android.accounts.Account;
 import android.accounts.AccountManager;
 import android.app.Activity;
+import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.ViewConfiguration;
 
 import th.pd.mail.fastsync.Const;
 import th.pd.mail.fastsync.MailProvider;
 import th.pd.mail.fastsync.Mailbox;
 
 public class MainActivity extends Activity {
+	private void createSyncAccount() {
+		Mailbox mailbox = Mailbox.fromQuery(0, null);
+		Account account = new Account(mailbox.getAddress(),
+				Const.ACCOUNT_TYPE);
+		AccountManager accountManager = (AccountManager) getSystemService(ACCOUNT_SERVICE);
+		accountManager.addAccountExplicitly(account, null, null);
+	}
+
+	private boolean isOutOfBounds(Context context, MotionEvent event) {
+		final int x = (int) event.getX();
+		final int y = (int) event.getY();
+		final int slop = ViewConfiguration.get(context)
+				.getScaledWindowTouchSlop();
+		final View decorView = getWindow().getDecorView();
+		return (x < -slop) || (y < -slop)
+				|| (x > (decorView.getWidth() + slop))
+				|| (y > (decorView.getHeight() + slop));
+	}
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -19,19 +42,19 @@ public class MainActivity extends Activity {
 		requestSyncMailFolder(13);
 	}
 
+	@Override
+	public boolean onTouchEvent(MotionEvent event) {
+		if (isOutOfBounds(this, event)) {
+			return moveTaskToBack(true);
+		}
+		return super.onTouchEvent(event);
+	}
+
 	private void requestSyncMailFolder(int internalIdOfMailFolder) {
 		Uri requestUri = Uri.parse(MailProvider.CONTENT_URI).buildUpon()
 				.appendEncodedPath(MailProvider.REQUEST_SYNC_MAIL_FOLDER)
 				.appendPath(Integer.toString(internalIdOfMailFolder))
 				.build();
 		getContentResolver().query(requestUri, null, null, null, null);
-	}
-
-	private void createSyncAccount() {
-		Mailbox mailbox = Mailbox.fromQuery(0, null);
-		Account account = new Account(mailbox.getAddress(),
-				Const.ACCOUNT_TYPE);
-		AccountManager accountManager = (AccountManager) getSystemService(ACCOUNT_SERVICE);
-		accountManager.addAccountExplicitly(account, null, null);
 	}
 }
--- mail/src/th/pd/mail/tidyface/compose/Hedwig.java
+++ mail/src/th/pd/mail/tidyface/compose/Hedwig.java
@@ -4,11 +4,17 @@ import android.app.Activity;
 import android.app.Fragment;
 import android.content.ActivityNotFoundException;
 import android.content.Intent;
+import android.content.res.Resources;
+import android.graphics.Point;
+import android.graphics.Rect;
 import android.net.Uri;
 import android.os.Bundle;
+import android.view.Display;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.WindowManager;
 import android.widget.Toast;
 
 import th.common.widget.TitlebarController;
@@ -16,6 +22,115 @@ import th.pd.mail.R;
 
 public class Hedwig extends Fragment implements
 		TitlebarController.Listener, ComposeController.Listener {
+
+	private class MoveListener implements View.OnTouchListener {
+		private final int[] MARGIN;
+		private final View mDecorView;
+		private WindowManager.LayoutParams mLayoutParams;
+
+		// i don't believe any resize triggered during moving
+		private Rect mAcceptableAnchorRect = new Rect();
+
+		// the center point of the window
+		private int[] mAnchor = new int[2];
+
+		private int[] mTouchPointFromAnchor = new int[2];
+
+		public MoveListener() {
+			mDecorView = getActivity().getWindow().getDecorView();
+			Resources res = getActivity().getResources();
+			MARGIN = new int[] {
+					res.getDimensionPixelOffset(R.dimen.compose_window_move_margin_top),
+					res.getDimensionPixelOffset(R.dimen.compose_window_move_margin_right),
+					res.getDimensionPixelOffset(R.dimen.compose_window_move_margin_bottom),
+					res.getDimensionPixelOffset(R.dimen.compose_window_move_margin_left)
+			};
+		}
+
+		private void findAndSetAcceptableAnchorRect(View handleView) {
+			Display defDisplay = getActivity().getWindowManager()
+					.getDefaultDisplay();
+			Point screenSize = new Point();
+			defDisplay.getSize(screenSize);
+
+			// the boundary for the anchor point;
+			// for determine whether the window goes beyoud the boundary
+			mAcceptableAnchorRect.top = -handleView.getTop() + MARGIN[0];
+			mAcceptableAnchorRect.right =
+					screenSize.x - handleView.getRight() - MARGIN[1];
+			mAcceptableAnchorRect.bottom =
+					screenSize.y - handleView.getBottom() - MARGIN[2];
+			mAcceptableAnchorRect.left = -handleView.getLeft() + MARGIN[3];
+			mAcceptableAnchorRect.offset(mDecorView.getWidth() / 2,
+					mDecorView.getHeight() / 2);
+		}
+
+		/**
+		 * take the center point as anchor point
+		 */
+		private void findAndSetWindowAnchorPoint(int[] anchor) {
+			mDecorView.getLocationOnScreen(anchor);
+			anchor[0] += mDecorView.getWidth() / 2;
+			anchor[1] += mDecorView.getHeight() / 2;
+		}
+
+		private void onMove(int rawX, int rawY) {
+			int anchorX = rawX - mTouchPointFromAnchor[0];
+			int anchorY = rawY - mTouchPointFromAnchor[1];
+
+			if (anchorX < mAcceptableAnchorRect.left) {
+				anchorX = mAcceptableAnchorRect.left;
+			} else if (anchorX > mAcceptableAnchorRect.right) {
+				anchorX = mAcceptableAnchorRect.right;
+			}
+
+			if (anchorY < mAcceptableAnchorRect.top) {
+				anchorY = mAcceptableAnchorRect.top;
+			} else if (anchorY > mAcceptableAnchorRect.bottom) {
+				anchorY = mAcceptableAnchorRect.bottom;
+			}
+
+			// the offset after adjust
+			int dx = anchorX - mAnchor[0];
+			int dy = anchorY - mAnchor[1];
+
+			mLayoutParams = (WindowManager.LayoutParams) mDecorView
+					.getLayoutParams();
+			mLayoutParams.x += dx;
+			mLayoutParams.y += dy;
+			mLayoutParams.flags |= WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS;
+			getActivity().getWindowManager().updateViewLayout(
+					mDecorView, mLayoutParams);
+
+			mAnchor[0] += dx;
+			mAnchor[1] += dy;
+		}
+
+		@Override
+		public boolean onTouch(View v, MotionEvent event) {
+			final int rawX = (int) event.getRawX();
+			final int rawY = (int) event.getRawY();
+			event.getX();
+
+			switch (event.getAction() & MotionEvent.ACTION_MASK) {
+				case MotionEvent.ACTION_DOWN:
+					findAndSetWindowAnchorPoint(mAnchor);
+					findAndSetAcceptableAnchorRect(v);
+					mTouchPointFromAnchor[0] = rawX - mAnchor[0];
+					mTouchPointFromAnchor[1] = rawY - mAnchor[1];
+					break;
+				case MotionEvent.ACTION_POINTER_DOWN:
+					break;
+				case MotionEvent.ACTION_UP:
+				case MotionEvent.ACTION_POINTER_UP:
+					break;
+				case MotionEvent.ACTION_MOVE:
+					onMove(rawX, rawY);
+					break;
+			}
+			return true;
+		}
+	}
 
 	private static final int REQUEST_CODE_PICK_FILE = 11;
 
@@ -31,6 +146,8 @@ public class Hedwig extends Fragment implements
 				onSendMail();
 			}
 		});
+		getView().findViewById(R.id.titlebar).setOnTouchListener(
+				new MoveListener());
 	}
 
 	@Override
@@ -46,6 +163,11 @@ public class Hedwig extends Fragment implements
 				break;
 		}
 		super.onActivityResult(requestCode, resultCode, data);
+	}
+
+	@Override
+	public void onCleanExit() {
+		this.getActivity().finish();
 	}
 
 	@Override
@@ -69,23 +191,17 @@ public class Hedwig extends Fragment implements
 	}
 
 	@Override
-	public void onCleanExit() {
-		this.getActivity().finish();
-	}
-
-	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 			Bundle savedInstanceState) {
+		// make sure the default window padding is cleared
+		getActivity().getWindow().setBackgroundDrawable(null);
+
 		View view = inflater.inflate(R.layout.hedwig, container, false);
 		TitlebarController.newInstance(view, this);
 		mComposeController = ComposeController.newInstance(view, this);
 		mBtnSend = view.findViewById(R.id.btnSend);
-		return view;
-	}
 
-	private void onSendMail() {
-		ComposeModel model = mComposeController.removeCurrentTab();
-		// TODO
+		return view;
 	}
 
 	@Override
@@ -104,5 +220,10 @@ public class Hedwig extends Fragment implements
 					R.string.cannot_find_app_to_pick_file, Toast.LENGTH_SHORT)
 					.show();
 		}
+	}
+
+	private void onSendMail() {
+		ComposeModel model = mComposeController.removeCurrentTab();
+		// TODO
 	}
 }

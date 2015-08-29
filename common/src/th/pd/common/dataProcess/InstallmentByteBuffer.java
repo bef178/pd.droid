93ad0236f8520936f33218c59244f7cda9f383d3
tanghao <th4416@hotmail.com>
2015-08-29 10:56:12 +0800

    change internal storage of InstallmentByteBuffer
    avoid copy data bytes when enlarge capacity

--- common/src/th/pd/common/dataProcess/InstallmentByteBuffer.java
+++ common/src/th/pd/common/dataProcess/InstallmentByteBuffer.java
@@ -1,18 +1,21 @@
 package th.pd.common.dataProcess;
 
+import java.io.IOException;
+import java.io.OutputStream;
 import java.io.UnsupportedEncodingException;
+import java.util.ArrayList;
 import java.util.Arrays;
 
 /**
  * some thing like smart array & queue & installment savings
  */
-public class InstallmentByteBuffer {
+public class InstallmentByteBuffer extends OutputStream {
 
     private static final int INSTALLMENT_BITS = 10;
     private static final int INSTALLMENT_BYTES = 1 << INSTALLMENT_BITS;
     private static final int INSTALLMENT_MASK = INSTALLMENT_BYTES - 1;
 
-    private byte[] savings;
+    private ArrayList<byte[]> savings = new ArrayList<>();
     private int used = 0;
 
     private int reader = 0;
@@ -20,25 +23,18 @@ public class InstallmentByteBuffer {
     private boolean readonly = false;
 
     public InstallmentByteBuffer() {
-        this(1 << INSTALLMENT_BITS);
+        this(INSTALLMENT_BYTES);
     }
 
     public InstallmentByteBuffer(int capacity) {
         setupCapacity(capacity);
     }
 
-    public InstallmentByteBuffer(byte[] a, int used, boolean readonly) {
-        assert a != null;
-        this.savings = a;
-        this.used = used;
-        this.readonly = readonly;
-    }
-
-    public InstallmentByteBuffer append(byte b) {
-		if (!readonly()) {
-			setupCapacity(used + 1);
-			savings[used++] = b;
-		}
+    public InstallmentByteBuffer append(int b) {
+        if (!readonly()) {
+            setupCapacity(used + 1);
+            put(used++, (byte) (b & 0x3F));
+        }
         return this;
     }
 
@@ -51,9 +47,37 @@ public class InstallmentByteBuffer {
             return this;
         }
 
-        setupCapacity(used + a.length);
-        System.arraycopy(a, start, savings, used, length);
-        used += length;
+        setupCapacity(used + length);
+
+        int i = start;
+        int j = start + length;
+
+        if ((used & INSTALLMENT_MASK) != 0) {
+            int n = INSTALLMENT_BYTES - (used & INSTALLMENT_MASK);
+            if (n > length) {
+                n = length;
+            }
+            System.arraycopy(a, i,
+                    savings.get(used >> INSTALLMENT_BITS), used
+                            & INSTALLMENT_MASK, n);
+            i += n;
+            used += n;
+        }
+
+        while (i + INSTALLMENT_BYTES < j) {
+            System.arraycopy(a, i,
+                    savings.get(used >> INSTALLMENT_BITS), 0,
+                    INSTALLMENT_BYTES);
+            i += INSTALLMENT_BYTES;
+            used += INSTALLMENT_BYTES;
+        }
+
+        if (i < j) {
+            System.arraycopy(a, i,
+                    savings.get(used >> INSTALLMENT_BITS), 0, j - i);
+            used += j - i;
+        }
+
         return this;
     }
 
@@ -61,18 +85,24 @@ public class InstallmentByteBuffer {
      * @return a copy of valid in bounds byte array
      */
     private byte[] array() {
-        return Arrays.copyOf(savings, used);
-    }
-
-    /**
-     * @return the inner byte array
-     */
-    public byte[] bytes() {
-        return savings;
+        byte[] array = new byte[used];
+        int i = 0;
+        while (i + INSTALLMENT_BYTES <= used) {
+            System.arraycopy(savings.get(i >> INSTALLMENT_BITS), 0, array,
+                    i, INSTALLMENT_BYTES);
+            i += INSTALLMENT_BYTES;
+        }
+        System.arraycopy(savings.get(i >> INSTALLMENT_BITS), 0, array, i,
+                used - i);
+        return array;
     }
 
     public int capacity() {
-        return savings.length;
+        return savings.size() << INSTALLMENT_BITS;
+    }
+
+    private int get(int pos) {
+        return savings.get(pos >> INSTALLMENT_BITS)[pos & INSTALLMENT_MASK];
     }
 
     public boolean hasNext() {
@@ -83,11 +113,11 @@ public class InstallmentByteBuffer {
         return used == 0;
     }
 
-    public byte next() {
+    public int next() {
         if (hasNext()) {
-            return savings[reader++];
+            return get(reader++) & 0xFF;
         } else {
-            throw new IndexOutOfBoundsException();
+            return -1;
         }
     }
 
@@ -109,12 +139,16 @@ public class InstallmentByteBuffer {
     /**
      * may throw IndexOutOfBoundsException
      */
-    public byte peek() {
+    public int peek() {
         if (hasNext()) {
-            return savings[reader];
+            return get(reader) & 0xFF;
         } else {
-            throw new IndexOutOfBoundsException();
+            return -1;
         }
+    }
+
+    private void put(int pos, byte b) {
+        savings.get(pos >> INSTALLMENT_BITS)[pos & INSTALLMENT_MASK] = b;
     }
 
     public boolean readonly() {
@@ -141,12 +175,12 @@ public class InstallmentByteBuffer {
         used = 0;
     }
 
-    public void reset(int n) {
+    public void reset(int offset) {
         if (readonly) {
             return;
         }
-        if (n >= 0 && n < used) {
-            used -= n;
+        if (offset >= 0 && offset < used) {
+            used -= offset;
         } else {
             throw new IllegalArgumentException();
         }
@@ -161,15 +195,25 @@ public class InstallmentByteBuffer {
 
     private void setupCapacity(int newLength) {
         if (savings == null) {
-            savings = new byte[0];
+            savings = new ArrayList<>();
         }
-        if (newLength > savings.length) {
-            int newCapacity = newLength & ~INSTALLMENT_MASK;
-            if (newCapacity < newLength) {
-                newCapacity += INSTALLMENT_BYTES;
+        if (newLength > capacity()) {
+            int n = newLength >> INSTALLMENT_BITS;
+            if ((newLength & INSTALLMENT_MASK) != 0) {
+                ++n;
             }
-            savings = Arrays.copyOf(savings, newCapacity);
+            for (int i = savings.size() + 1; i <= n; ++i) {
+                savings.add(new byte[INSTALLMENT_BYTES]);
+            }
         }
+    }
+
+    public int size() {
+        return used;
+    }
+
+    public byte[] toByteArray() {
+        return array();
     }
 
     @Override
@@ -189,7 +233,14 @@ public class InstallmentByteBuffer {
         if (readonly) {
             return;
         }
-        Arrays.fill(savings, (byte) 0);
+        for (byte[] a : savings) {
+            Arrays.fill(a, (byte) 0);
+        }
         used = 0;
+    }
+
+    @Override
+    public void write(int oneByte) throws IOException {
+        append(oneByte);
     }
 }

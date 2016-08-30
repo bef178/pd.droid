231506f1c895a1454c3e39cfcb07463feffd194e
tanghao <tanghao.hb@gmail.com>
2016-08-31 01:08:05 +0800

    rename build var for clarity

--- build/f.mk
+++ build/f.mk
@@ -1,21 +1,26 @@
 ########################################
 # f.mk
+#
+# ANDROID_HOME
+# KEYSTORE
+# KEYSTORE_PASS
+# KEY_NAME
 
-#ANDROID_TARGET_SDK ?= $(shell ls $(ANDROID_HOME)/platforms | sort -nr -k 2 -t - | head -1)
-ANDROID_TARGET_SDK ?= android-22
+#ANDROID_API ?= $(shell ls $(ANDROID_HOME)/platforms | sort -nr -k 2 -t - | head -1)
+ANDROID_API ?= android-22
 
-ANDROID_BUILD_TOOLS ?= $(shell ls $(ANDROID_HOME)/build-tools | sort -nr | head -1)
+ANDROID_BUILD_BIN ?= $(shell ls $(ANDROID_HOME)/build-tools | sort -nr | head -1)
 
 ########
 
-ANDROID_JAR := $(ANDROID_HOME)/platforms/$(ANDROID_TARGET_SDK)/android.jar
+ANDROID_JAR := $(ANDROID_HOME)/platforms/$(ANDROID_API)/android.jar
 $(info using $(ANDROID_JAR))
 
-ANDROID_BUILD_TOOLS := $(ANDROID_HOME)/build-tools/$(ANDROID_BUILD_TOOLS)
-$(info using $(ANDROID_BUILD_TOOLS))
+ANDROID_BUILD_BIN := $(ANDROID_HOME)/build-tools/$(ANDROID_BUILD_BIN)
+$(info using $(ANDROID_BUILD_BIN))
 
-AAPT := $(ANDROID_BUILD_TOOLS)/aapt
-DX   := $(ANDROID_BUILD_TOOLS)/dx
+AAPT := $(ANDROID_BUILD_BIN)/aapt
+DX   := $(ANDROID_BUILD_BIN)/dx
 
 ########
 
@@ -23,13 +28,13 @@ define sign_jar
     @jarsigner \
         $(shell if test "$(3)" != "true"; then echo "-tsa http://timestamp.digicert.com"; fi) \
         -digestalg SHA1 -sigalg MD5withRSA \
-        -keystore $(ANDROID_KEYSTORE) -storepass $(ANDROID_KEYSTORE_PASS) \
-        -signedjar $(2) -sigfile cert $(1) $(ANDROID_KEYSTORE_ALIAS)
+        -keystore $(KEYSTORE) -storepass $(KEYSTORE_PASS) \
+        -signedjar $(2) -sigfile cert $(1) $(KEY_NAME)
 endef
 
 define sign_apk
     $(call sign_jar,$(1),$(2).signed,$(3))
-    @$(ANDROID_BUILD_TOOLS)/zipalign -f 4 $(1) $(2)
+    @$(ANDROID_BUILD_BIN)/zipalign -f 4 $(1) $(2)
 endef
 
 define find_typef

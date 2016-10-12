04a190feb42202eb37bad5c2fa7617615da910c9
tanghao <tanghao.hb@gmail.com>
2016-10-12 19:55:26 +0800

    demo: add network interface summary

--- common/demo/src/t/typedef/droid/demo/DemoActivity.java
+++ common/demo/src/t/typedef/droid/demo/DemoActivity.java
@@ -1,7 +1,11 @@
 package t.typedef.droid.demo;
 
 import android.app.Activity;
+import android.content.Context;
 import android.graphics.Typeface;
+import android.net.ConnectivityManager;
+import android.net.Network;
+import android.net.NetworkInfo;
 import android.os.Bundle;
 import android.text.InputType;
 import android.view.KeyEvent;
@@ -11,6 +15,10 @@ import android.widget.Checkable;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 public class DemoActivity extends Activity {
 
     @Override
@@ -18,16 +26,18 @@ public class DemoActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.demo_activity);
 
-        showDensity();
+        showDensity((TextView) findViewById(R.id.densityText));
+        showNetworkInterfaceStat((TextView) findViewById(R.id.networkInterfaceStatText));
         setupPasswordView(R.id.editPassword, R.id.actionShowPassword, -1);
     }
 
-    private void showDensity() {
-        TextView densityText = (TextView) findViewById(R.id.densityText);
-        if (densityText != null) {
-            float density = getResources().getDisplayMetrics().density;
-            densityText.setText("density: " + density);
-        }
+    private void setEnabledVisiblePassword(boolean visible, EditText edit) {
+        int selStart = edit.getSelectionStart();
+        int selEnd = edit.getSelectionEnd();
+        edit.setInputType(InputType.TYPE_CLASS_TEXT
+                | (visible ? InputType.TYPE_TEXT_VARIATION_VISIBLE_PASSWORD
+                        : InputType.TYPE_TEXT_VARIATION_PASSWORD));
+        edit.setSelection(selStart, selEnd);
     }
 
     private void setupPasswordView(int resEdit, int resCheckable,
@@ -36,6 +46,7 @@ public class DemoActivity extends Activity {
         edit.setTypeface(Typeface.MONOSPACE);
         edit.setOnEditorActionListener(
                 new TextView.OnEditorActionListener() {
+
                     @Override
                     public boolean onEditorAction(TextView v, int actionId,
                             KeyEvent event) {
@@ -54,6 +65,7 @@ public class DemoActivity extends Activity {
 
         findViewById(resCheckable).setOnClickListener(
                 new View.OnClickListener() {
+
                     @Override
                     public void onClick(View v) {
                         Checkable checkable = (Checkable) v;
@@ -64,12 +76,72 @@ public class DemoActivity extends Activity {
                 });
     }
 
-    private void setEnabledVisiblePassword(boolean visible, EditText edit) {
-        int selStart = edit.getSelectionStart();
-        int selEnd = edit.getSelectionEnd();
-        edit.setInputType(InputType.TYPE_CLASS_TEXT
-                | (visible ? InputType.TYPE_TEXT_VARIATION_VISIBLE_PASSWORD
-                        : InputType.TYPE_TEXT_VARIATION_PASSWORD));
-        edit.setSelection(selStart, selEnd);
+    private void showDensity(TextView densityText) {
+        if (densityText != null) {
+            float density = getResources().getDisplayMetrics().density;
+            densityText.setText("density: " + density);
+        }
+    }
+
+    private void showNetworkInterfaceStat(TextView connText) {
+        if (connText != null) {
+            connText.setTypeface(Typeface.MONOSPACE);
+            try {
+                connText.setText("networkInterfaceStat: "
+                        + sumUpNetworkInterfaceStat(this).toString(2));
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private JSONObject sumUpNetworkInterfaceStat(Context context)
+            throws JSONException {
+        ConnectivityManager connManger = (ConnectivityManager) context
+                .getSystemService(Context.CONNECTIVITY_SERVICE);
+
+        JSONObject stat = new JSONObject();
+
+        JSONObject active = toJson(connManger
+                .getActiveNetworkInfo());
+        active.put("metered", connManger.isActiveNetworkMetered());
+        stat.put("active", active);
+
+        Network[] networks = connManger.getAllNetworks();
+        if (networks != null) {
+            JSONArray list = new JSONArray();
+            for (Network network : networks) {
+                NetworkInfo networkInfo = connManger.getNetworkInfo(network);
+                list.put(toJson(networkInfo));
+            }
+            stat.put("all", list);
+        }
+
+        JSONArray typeList = new JSONArray();
+        for (int type : new Integer[] {
+                ConnectivityManager.TYPE_MOBILE,
+                ConnectivityManager.TYPE_WIFI,
+                ConnectivityManager.TYPE_ETHERNET
+        }) {
+            NetworkInfo networkInfo = connManger.getNetworkInfo(type);
+            if (networkInfo != null) {
+                if (networkInfo.isAvailable()) {
+                    typeList.put(toJson(networkInfo));
+                }
+            }
+        }
+        return stat;
+    }
+
+    private JSONObject toJson(NetworkInfo networkInfo)
+            throws JSONException {
+        JSONObject stat = new JSONObject();
+        if (networkInfo != null) {
+            stat.put("state", networkInfo.getState().name());
+            stat.put("type", networkInfo.getTypeName());
+            stat.put("subtype", networkInfo.getSubtypeName());
+            stat.put("available", networkInfo.isAvailable());
+        }
+        return stat;
     }
 }
